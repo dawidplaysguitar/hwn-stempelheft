0827664d7b56adcef9d705c3a9951b3ac4f564e7
dawidplaysguitar <mikler1303@gmail.com>
2026-06-24 17:26:50 +0200

    V.1.0.0
    Start der Reise dieser Webapp:
    - Login Funktion
    - Grafiken für Abzeichen
    - Tabelle mit Stempeln und anzeige welche abgeschlossen sind

---
+++ auth.js
@@ -0,0 +1,21 @@
+import { auth }
+from "./firebase-config.js";
+
+import {
+    onAuthStateChanged
+}
+from
+"https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+onAuthStateChanged(
+auth,
+(user) => {
+
+    if(!user) {
+
+        window.location.href =
+        "login.html";
+
+    }
+
+});
